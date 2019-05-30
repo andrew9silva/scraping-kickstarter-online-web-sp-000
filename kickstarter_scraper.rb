4256728688cb7e83867a537047873f2279b62447
Andrew Silva <andrewsilva9@gmail.com>
2019-05-30 01:08:22 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,6 +11,7 @@ def create_project_hash
   # projects: kickstarter.css("li.project.grid_4")
   # project.css("h2.bbcard_name strong a").text
   # project.css("div.project-thumbnail a img").attribute("src").value
+  # project.css("p.bbcard_blurb").text
   # write your code here
 end
 
